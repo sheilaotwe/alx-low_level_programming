6041e3226c834cca23d2aeace2d53077e74cf7c3
sheilaotwe <so3cg@virginia.edu>
2022-06-07 20:08:37 -0700

    Print alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,8 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar ('\n');
 	}
 }
