12788a55ee6a986ad34ebf2137bdbc5c74f088d1
sheilaotwe <so3cg@virginia.edu>
2022-06-11 16:45:36 -0700

    Print combination of 3 numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Print combinations of three digits
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int digit1, digit2, digit3;
+
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
+	{
+		for (digit2 = '0'; digit2 <= '9'; digit2++)
+		{
+			for (digit3 = '0'; digit3 <= '9'; digit3++)
+			{
+				if (digit1 < digit2 && digit2 < digit3)
+				{
+
+					putchar(digit1);
+			putchar(digit2);
+			putchar(digit3);
+
+			if (c != '7')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
