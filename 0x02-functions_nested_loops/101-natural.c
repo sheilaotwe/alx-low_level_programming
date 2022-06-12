ac119773cdd731f4016fb938604f6850d658a769
sheilaotwe <so3cg@virginia.edu>
2022-06-11 17:53:11 -0700

    Check for multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check for multiple of 3 and 5
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int a = 1024, b, sum = 0;
+
+	for (b = 0; b < a; b++)
+	{
+		if ((b % 3 == 0) || (b % 5 ==0))
+		{
+			sum = sum + b;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
