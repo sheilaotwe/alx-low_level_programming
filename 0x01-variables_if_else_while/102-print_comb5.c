936f9ac2d59334916a7e6d6b8d3125158792f5c0
sheilaotwe <so3cg@virginia.edu>
2022-06-11 17:05:29 -0700

    Print combinations of two digits of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Print combination of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 100; digit1++)
+	{
+		for (digit2 = 0; digit2 < 100; digit2++)
+		{
+			if (digit1 < digit2)
+			{
+				putchar((digit1 / 10) + 48);
+				putchar((digit1 % 10) + 48);
+				putchar(' ');
+				putchar((digit2 / 10) + 48);
+				putchar((digit2 / 10) + 48);
+				if (digit1 != 98 || digit2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
