0b460c6ca1fe7b7753c56ac1b7aceb3ea1c14415
sheilaotwe <so3cg@virginia.edu>
2022-06-11 17:36:55 -0700

    Print times table of the input

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_times_table - Print times table starting with 0
+ * @n: value of the times table to be printed
+ */
+void print_times_table(int n)
+{
+	int num1, num2, num3;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num1 = 0; num1 <= n; num1++)
+		{
+			_putchar('0');
+
+			for (num2 = 0; num2 <= n; num2++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				num3 = num1 * num2;
+
+				if (num3 <= 99)
+					_putchar(' ');
+				if (num3 <= 9)
+					_putchar(' ');
+
+				if (num3 >= 100)
+				{
+					_putchar((num3 / 100) + '0');
+					_putchar((num3 / 10) % 10 + '0');
+				}
+				else if (num3 <= 99 && num3 >= 10)
+				{
+					_putchar((num3 / 10) + '0');
+				}
+				_putchar((num3 % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
