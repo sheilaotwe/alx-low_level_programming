dc8a618ac9f8b0b9ef88fa9a5b3633f94ae815b1
sheilaotwe <so3cg@virginia.edu>
2022-06-14 17:40:48 -0700

    Swap the value of two integers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * rest_to_98 - updates the value *n points to
+ * reset_to_98 - updates the value *n points to
  * @n: pointer to an int
- * Return: (0)
  */
 
 void reset_to_98(int *n)
