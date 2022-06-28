51bc1d276bb614b8a9a1353c45405b753b8414ee
sheilaotwe <so3cg@virginia.edu>
2022-06-27 17:15:58 -0700

    Function for grid of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Function that returns a pointer to a 2D array of integers
+ * @width: number of columns
+ * @height: number of rows
+ * Return: pointer to grid or NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, a, b;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (a = 0; a < height; a++)
+	{
+		grid[a] = malloc(sizeof(int) * width);
+		if (grid[a] == NULL)
+		{
+			while (--a >= 0)
+				free(grid[a]);
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+			grid[a][b] = 0;
+	}
+	return (grid);
+}
