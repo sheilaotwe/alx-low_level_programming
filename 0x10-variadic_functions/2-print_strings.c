15faed6c20e23ba173189acd4b633c34926565bf
sheilaotwe <so3cg@virginia.edu>
2022-07-07 17:39:29 -0700

    Fixing file 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			}
 			else
 			{
-				printf("%s", nil_check(va_arg(args, char *)))
+				printf("%s", nil_check(va_arg(args, char *)));
 			}
 		}
 	}
