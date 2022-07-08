3028c57dbdcd26b31e504f2a62a3c838449cda99
sheilaotwe <so3cg@virginia.edu>
2022-07-07 18:25:01 -0700

    Program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function that adds two integers a and b
+ * @a: interger one
+ * @b: interger two
+ * Return: sum, a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function that subtracts two integers a and b
+ * @a: integer one
+ * @b: integer two
+ * Return: difference, a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function that multiples two integers a and b
+ * @a: integer one
+ * @b: integer two
+ * Return: product, a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b)
+}
+
+/**
+ * op_div - Function that divides two integers a and b
+ * @a: integer one
+ * @b: integer two
+ * Return: quotient, a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function that mods two integers a and b
+ * @a: integer one
+ * @b: integer two
+ * Return: mod, a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
