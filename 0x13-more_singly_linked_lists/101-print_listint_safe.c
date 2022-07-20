c824201a39dec6ff49c9ac675586ab788f4cbd1f
sheilaotwe <so3cg@virginia.edu>
2022-07-19 19:27:31 -0700

    Function that prints a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * find_listint_loop_pl - Function that finds a loop in a linked list
+ * @head: linked list
+ * Return: address of node where loop, NULL if no loop
+ */
+listint_t *find_listint_loop_pl(listint_t *head)
+{
+	listint_t *ptr, *end;
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
+
+/**
+ * print_listint_safe - Function that prints a listint_t linked list
+ * even if it has loop
+ * @head: head of list
+ * Return: number of nodes printed
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t len = 0;
+	int loop;
+	listint_t *loopnode;
+	loopnode = find_listint_loop_pl((listint_t *) head);
+	for (len = 0, loop = 1; (head != loopnode || loop) && head != NULL; len++)
+	{
+		printf("[%p] %d\n", (void *) head, head->n);
+		if (head == loopnode)
+			loop = 0;
+		head = head->next;
+	}
+	if (loopnode != NULL)
+		printf("-> [%p] %d\n", (void *) head, head->n);
+	return (len);
+}
