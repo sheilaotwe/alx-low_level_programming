2a293b42221f30dd9f2cf871d8f0d6f4cb329448
sheilaotwe <so3cg@virginia.edu>
2022-08-11 15:10:34 -0700

    updating code for realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,8 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * _realloc - Function that reallocates memory block using malloc and free
- * @ptr: pointer
+ * @ptr: elements to allocate
  * @old_size: old size
  * @new_size: new size
  * Return: pointer
@@ -10,25 +12,45 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *relloc;
-	unsigned int i;
+	int j;
+	void *s;
 
-	if (ptr !: NULL)
-		clone = ptr;
-	else
-	{ return (malloc(new_size)); }
-	if (new_size == old_size)
+	if (old_size == new_size)
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{ free(ptr);
-		return (0); }
-	relloc = malloc(new_size);
-	if (relloc == NULL)
-		return(0);
-	for (i = 0; i < (old_size || i < new_size); i++)
+	if (ptr == NULL)
 	{
-		*(relloc + i) = clone[i];
+		ptr = malloc(new_size);
+		return (ptr);
 	}
-	free(ptr);
-	return (relloc);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr != NULL)
+	{
+		s = malloc(new_size);
+		for (j = 0; j < min(old_size, new_size); j++)
+		{
+			((char *)s)[j] = ((char *)ptr)[j];
+		}
+		free(ptr);
+	}
+	else
+		return (NULL);
+	return (s);
+}
+
+/**
+ * min - min of a and b
+ * @a: first
+ * @b: second
+ * Return: return minimum
+ */
+
+int min(unsigned int a, unsigned int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
 }
