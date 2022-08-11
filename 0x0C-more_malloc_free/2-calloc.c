0ef4f1f668f5433108a34dd65e60bd4231de3b5f
sheila.otwe <so3cg@virginia.edu>
2022-08-11 08:18:15 -0700

    fixing error with code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	memory == malloc(nmemb * size);
+	memory = malloc(nmemb * size);
 	if (memory == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
