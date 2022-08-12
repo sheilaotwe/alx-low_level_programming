7d61542c052b456cbe6777932c6550460e65a4c1
sheilaotwe <so3cg@virginia.edu>
2022-08-11 21:05:06 -0700

    move pointer to string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - function that moves pointer to string
+ * @s: pointer to pointer string
+ * @to: pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
