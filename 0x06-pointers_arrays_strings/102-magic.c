2efe3bda9fa5a375ec0a3b2e3aceba1038559554
sheilaotwe <so3cg@virginia.edu>
2022-08-11 20:34:47 -0700

    file for magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
-
+	
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write line of code here:
-	 * not allowed to use variable a in new line of code
-	 * not allowed to modify variable p
-	 * only write one statement
-	 * not allowed to code anything except this line of code
+	/**
+	 * line of code here
+	 * requirements:
+	 * not allowed to use a
+	 * not allowed to modify p
+	 * only one statement
+	 * not allowed to code anything else and this line
 	 */
 	p[5] = 98;
-	/*
-	 * print 98\n
+	/**
+	 * this print 98\n
 	 */
 	printf("a[2] = %d\n", a[2]);
-
 	return (0);
 }
