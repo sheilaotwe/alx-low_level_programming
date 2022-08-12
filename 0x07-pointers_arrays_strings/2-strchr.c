4529ff2cae8d74a70864fa5114dbddca1e40ea19
sheilaotwe <so3cg@virginia.edu>
2022-08-11 21:14:19 -0700

    updating file for 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,9 +7,10 @@
  * Return: pointer to the first occurence of character in string or 
  * pointer to null if character is not found
  */
+
 char *_strchr(char *s, char c)
 {
-	while (*s !== '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
@@ -17,5 +18,6 @@ char *_strchr(char *s, char c)
 			return (s + 1);
 		s++;
 	}
+
 	return (s + 1);
 }
